8b393529cf16a880cee1c5031cc27d6197f950b6
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-08-23 12:11:51 +0100

    add lougout functionality

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Sorry, site is currently undergoing maintenance. Please try again later.')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
