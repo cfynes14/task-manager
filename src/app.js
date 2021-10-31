2b43e3f27af299f7d4c306a62780f6ea6a5c0ca0
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-10-31 18:10:59 +0000

    add brackets to CORS

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(cors)
+app.use(cors())
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
