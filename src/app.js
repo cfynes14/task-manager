d3ddcaec0b01c3e92b7730842880ea9ae97d3ed0
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-10-31 18:49:59 +0000

    remove middleware

--- src/app.js
+++ src/app.js
@@ -7,18 +7,11 @@ const cors = require('cors')
 
 const app = express()
  
-app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-
-
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+app.use(cors())
 
 app.use(express.json())
 app.use(userRouter)
