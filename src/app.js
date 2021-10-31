546a23fbe8bb61ad3167bb539a2d06552aab9684
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-10-31 18:41:47 +0000

    move position of cors

--- src/app.js
+++ src/app.js
@@ -3,15 +3,16 @@ require('./db/mongoose');
 const cookieParser = require('cookie-parser')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const cors = require('cors')
+const cors = require('cors') 
 
 const app = express()
  
+app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(cors())
+
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
