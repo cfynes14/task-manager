180de5ecbc39ca0aba8908d89d9ac6883accbe72
Chris  Fynes <chris.fynes@fxdigital.uk>
2022-08-05 16:51:22 +0100

    add CLIENT_ENDPOINT variable

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_ENDPOINT); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
